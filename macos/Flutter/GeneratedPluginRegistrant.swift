200acbda2c7b96cf5accbbf6e77cddb05d8032be
maxterjunior <vecacriju@gmail.com>
2024-08-16 01:46:02 -0500

    chore: Update sqflite import in GeneratedPluginRegistrant.swift

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,12 @@ import flutter_blue_plus
 import flutter_local_notifications
 import path_provider_foundation
 import share_plus
-import sqflite 
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin")) 
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
